a4282e96345cd0a163c3ef7067680d8199a9cfb1
Antonio Robirosa <antonio@areko.consulting>
2023-11-15 16:50:25 +0200

    Query to identify a noun

---
+++ flashcardCreator/bulgarianDictionary/tests.sql
@@ -0,0 +1,5 @@
+SELECT * FROM bulgarianDictionary.derivative_form where name in ('мъж','мъже');
+select * from word where id = 85167;
+select * from word_type where id = 46;
+
+select * from word_type;

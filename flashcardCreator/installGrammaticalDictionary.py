f6d227379bda33701dcd2b0e91a87924a7421e78
Antonio Robirosa <antonio@areko.consulting>
2023-11-16 12:04:45 +0200

    Try to download the file. Not working

--- flashcardCreator/installGrammaticalDictionary.py
+++ flashcardCreator/installGrammaticalDictionary.py
@@ -22,7 +22,16 @@
 import configparser
 from getpass import getpass
 from mysql.connector import connect, Error
+import urllib.request
+import gzip
+import shutil
+import tempfile
 
+# The database file located at https://rechnik.chitanka.info/db.sql.gz can only be downloaded with an interactive browser.
+# It moved it to my own hosting. It has the GPL 2 license
+GRAMMATICAL_DATABASE_URL='https://files.areko.consulting/rechnik.chitanka.info.bulgarian.db.sql.gz'
+
+print("Checking if the grammatical database was already downloaded.")
 config = configparser.ConfigParser()
 config.read("configuration.ini")
 if 'GrammaticalDictionary.User' in config:
@@ -30,6 +39,13 @@ if 'GrammaticalDictionary.User' in config:
     exit(1)
 
 print('Downloading the database with the grammatical classification')
+urllib.request.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X ' \
+                                   'MetaSr 1.0'
+filehandle, _ = urllib.request.urlretrieve(GRAMMATICAL_DATABASE_URL)
+with gzip.open(filehandle, 'rb') as compressed_file:
+    with tempfile.NamedTemporaryFile(suffix='dict.sql', delete=False) as datatabase_dump:
+        print(f'Uncompressing {filehandle} to {datatabase_dump}')
+        shutil.copyfileobj(compressed_file, datatabase_dump)
 
 print('Please provide the credentials to connect to your MySQL server')
 database_host=input('Please enter the hostname or IP (Default is localhost): ')
@@ -64,4 +80,4 @@ print('Now we are going to create database with the grammatical classification')
 # create user bgDictionaryUser@localhost identified by 'changeme'
 # grant all on bgDictionary.* to bgDictionaryUser@localhost;
 # create database bulgarianDictionary;
-# https://rechnik.chitanka.info/db.sql.gz
+#
---
+++ flashcardCreator/start.py
@@ -0,0 +1,20 @@
+# import nltk
+# from nltk.corpus import stopwords
+# sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+# tokens = nltk.word_tokenize(sentence)
+# print(tokens)
+# tagged = nltk.pos_tag(tokens)
+# print(tagged)
+#
+# print(stopwords.fileids())
+
+
+# import argparse
+#
+# parser = argparse.ArgumentParser(
+#     prog='flashcardCreatorBG',
+#     description='Selects vocabulary and generates flashcards for studing')
+# parser.add_argument('-d', '--dictionary-database', type=str, default='')
+# args = parser.parse_args()
+# print(args)
+# print('Number of lines:', args.lines)

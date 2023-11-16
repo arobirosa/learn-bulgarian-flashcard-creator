581f938624155f64cba1951c2a072b6b37ea7a35
Antonio Robirosa <antonio@areko.consulting>
2023-11-16 14:49:16 +0200

    Now the database's configuration is stored in the config file. Now the code is divide in functions

--- flashcardCreator/installGrammaticalDictionary.py
+++ flashcardCreator/installGrammaticalDictionary.py
@@ -27,66 +27,98 @@ import gzip
 import shutil
 import tempfile
 
+# TODO Add logging replacing the print calls
+
 # The database file located at https://rechnik.chitanka.info/db.sql.gz can only be downloaded with an interactive browser.
 # It moved it to my own hosting. It has the GPL 2 license
+CONFIG_FILENAME = 'configuration.ini'
 GRAMMATICAL_DATABASE_URL = 'https://files.areko.consulting/rechnik.chitanka.info.bulgarian.db.sql.gz'
 
-print("Checking if the grammatical database was already downloaded.")
-config = configparser.ConfigParser()
-config.read("configuration.ini")
-if 'GrammaticalDictionary.User' in config:
-    print('The connection to the database containing the grammatical dictionary was already configured')
+
+def create_database_configuration():
+    """
+    Asks for the database parameters and saves the configuration on disk if the connection was successful
+    :return:True if there were no errors
+    """
+    config = configparser.ConfigParser(interpolation=None)
+    config.read(CONFIG_FILENAME)
+    if 'GrammaticalDictionary.User' in config:
+        print('The connection to the database containing the grammatical dictionary was already configured')
+        return True
+
+    print('Please provide the credentials to connect to your MySQL server')
+    database_host=input('Please enter the hostname or IP (Default is localhost): ')
+    if not database_host:
+        database_host = 'localhost'
+    database_port_str=input('Please enter the port (Default is 3336): ')
+    if database_port_str:
+        database_port = int(database_port_str)
+    else:
+        database_port = 3306
+    database_user=input('Please enter the username: ')
+    if not database_user:
+        print('No user was entered. Exiting')
+        return False
+    database_password=getpass('Please enter the password of the user: ')
+    if not database_password:
+        print('No password was entered. Exiting')
+        return False
+
+    print('Attempting to connect to the database server')
+    try:
+        with connect(host=database_host, port=database_port, user=database_user, password=database_password):
+            print('Connection SUCCESSFUL.')
+    except Error as e:
+        print('The connection failed:', e)
+        return False
+
+    print("Saving the database connection information in the configuration file")
+    config.add_section("GrammaticalDictionary")
+    config['GrammaticalDictionary']['Host']= database_host
+    config['GrammaticalDictionary']['Port']= str(database_port)
+    config['GrammaticalDictionary']['User']= database_user
+    config['GrammaticalDictionary']['Password'] = database_password
+    with open('configuration.ini', 'w') as configuration_file:
+        config.write(configuration_file)
+
+    return True
+
+
+def was_grammar_database_imported():
+    pass
+
+
+def download_import_grammar_database():
+    print("Checking if the grammatical database was already downloaded.")
+    if was_grammar_database_imported():
+        print('The grammatical database was already imported')
+        return True
+
+    print('Downloading the database with the grammatical classification')
+    auth_handler = urllib.request.HTTPBasicAuthHandler()
+    # TODO Remove credentials from code and change them on server
+    auth_handler.add_password(realm='please enter user and password',
+                              uri='https://files.areko.consulting',
+                              user='reader',
+                              passwd='DQaGtM6LK3VNXras')
+    opener = urllib.request.build_opener(auth_handler)
+    urllib.request.install_opener(opener)
+
+    with urllib.request.urlopen(GRAMMATICAL_DATABASE_URL) as url_downloader_response, gzip.GzipFile(fileobj=url_downloader_response) as compressed_file:
+        print(f'Downloading {GRAMMATICAL_DATABASE_URL}. Please wait 3-5 minutes')
+        uncompressed_dbdump_data = compressed_file.read()
+
+        with tempfile.NamedTemporaryFile(suffix='dict.sql', delete=False) as uncompressed_file:
+            print(f'Saving data to {uncompressed_file.name}')
+            uncompressed_file.write(uncompressed_dbdump_data)
+
+    print('Now we are going to create database with the grammatical classification')
+
+
+# Main body
+if not create_database_configuration():
     exit(1)
-
-print('Downloading the database with the grammatical classification')
-auth_handler = urllib.request.HTTPBasicAuthHandler()
-# TODO Remove credentials from code and change them on server
-auth_handler.add_password(realm='please enter user and password',
-                          uri='https://files.areko.consulting',
-                          user='reader',
-                          passwd='DQaGtM6LK3VNXras')
-opener = urllib.request.build_opener(auth_handler)
-urllib.request.install_opener(opener)
-
-with urllib.request.urlopen(GRAMMATICAL_DATABASE_URL) as url_downloader_response, gzip.GzipFile(fileobj=url_downloader_response) as compressed_file:
-    print(f'Downloading {GRAMMATICAL_DATABASE_URL}. Please wait 3-5 minutes')
-    uncompressed_dbdump_data = compressed_file.read()
-
-    with tempfile.NamedTemporaryFile(suffix='dict.sql', delete=False) as uncompressed_file:
-        print(f'Saving data to {uncompressed_file.name}')
-        uncompressed_file.write(uncompressed_dbdump_data)
-
-print('Please provide the credentials to connect to your MySQL server')
-database_host=input('Please enter the hostname or IP (Default is localhost): ')
-if not database_host:
-    database_host = 'localhost'
-database_port_str=input('Please enter the port (Default is 3336): ')
-if database_port_str:
-    database_port = int(database_port_str)
+elif not download_import_grammar_database():
+    exit(2)
 else:
-    database_port = 3306
-database_user=input('Please enter the username with rights to create databases and users: ')
-if not database_user:
-    print('No user was entered. Exiting')
-    exit(3)
-database_password=getpass('Please enter the password of the user with rights to create databases and users: ')
-if not database_password:
-    print('No password was entered. Exiting')
-    exit(4)
-
-print('Attempting to connect to the database server')
-try:
-    with connect(host=database_host, port=database_port, user=database_user, password=database_password) as database_connection:
-        print('Connection SUCCESSFUL.')
-except Error as e:
-    print('The connection failed:', e)
-    exit(5)
-
-print('Now we are going to create database with the grammatical classification')
-
-
-
-# create user bgDictionaryUser@localhost identified by 'changeme'
-# grant all on bgDictionary.* to bgDictionaryUser@localhost;
-# create database bulgarianDictionary;
-#
+    print('Now you can start using the flash creator')

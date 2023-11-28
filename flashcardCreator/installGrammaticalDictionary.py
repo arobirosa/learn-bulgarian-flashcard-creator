a6b3eef4f9915138f376177f82906abef8efe4ad
Antonio Robirosa <antonio@areko.consulting>
2023-11-28 17:21:57 +0200

    5 Now most of the TODO are tickets

--- flashcardCreator/installGrammaticalDictionary.py
+++ flashcardCreator/installGrammaticalDictionary.py
@@ -26,7 +26,6 @@ from os.path import exists
 
 # TODO Add logging replacing the print calls
 # TODO Move this module to a package in the main application
-# TODO Internationalize user inputs
 
 # The database file located at https://rechnik.chitanka.info/db.sql.gz can only be downloaded with an interactive browser.
 # It moved it to my own hosting. It has the GPL 2 license

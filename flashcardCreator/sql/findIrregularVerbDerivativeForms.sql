b58f51336b458a0062c73d7750beea47cb0e7f54
Antonio Robirosa <19760446+arobirosa@users.noreply.github.com>
2023-12-06 16:09:43 +0200

    10 Now all the intransitive terminative verbs are configured

--- flashcardCreator/sql/findIrregularVerbDerivativeForms.sql
+++ flashcardCreator/sql/findIrregularVerbDerivativeForms.sql
@@ -204,3 +204,64 @@ from derivative_form df2s
                         and df_other.name <> df2s.name || 'ият'
 where df2s.description = 'сег.деят.прич. м.р.'
 ;
+
+
+/** word_types with irregularities except for the verb tenses
+
+      # Сегашно време
+    'сег.вр., 1л., ед.ч.',
+    'сег.вр., 2л., ед.ч.',
+    'сег.вр., 3л., мн.ч.',  # Some "я" mutate to "е" or "а" like ям
+    # Минало свършено време (аорист)
+    'мин.св.вр., 1л., ед.ч.',
+    'мин.св.вр., 2л., ед.ч.',
+    # Минало несвършено време (имперфект)
+    'мин.несв.вр., 1л., ед.ч.',
+    'мин.несв.вр., 2л., ед.ч.',
+ */
+
+select word_from_word_type.speech_part, word_from_word_type.id word_type_id, w2.name,
+       df.name "повелително наклонение, ед.ч.", df2.name "повелително наклонение, мн.ч."
+from (select wt.speech_part, wt.id, min(w.id) word_id_max
+      from word_type wt
+               join word w on wt.id = w.type_id
+      where speech_part = 'verb_intransitive_terminative'
+      group by wt.speech_part, wt.id) word_from_word_type
+join word w2
+    on word_from_word_type.word_id_max = w2.id
+join derivative_form df
+    on w2.id = df.base_word_id
+    and df.description = 'повелително наклонение, ед.ч.'
+join derivative_form df2
+     on w2.id = df2.base_word_id
+         and df2.description = 'повелително наклонение, мн.ч.'
+where substr(w2.name, 0, length(w2.name)) || 'и' <> df.name
+and substr(w2.name, 0, length(w2.name)) || 'й' <> df.name
+order by word_from_word_type.speech_part, word_from_word_type.id ;
+
+
+select word_from_word_type.speech_part, word_from_word_type.id word_type_id, w2.name, df.name "мин.св.вр., 2л., ед.ч.",
+       df3.name "мин.деят.св.прич. м.р.", df4.name "мин.деят.св.прич. мн.ч.",
+       df5.name "мин.деят.несв.прич. м.р."
+from (select wt.speech_part, wt.id, min(w.id) word_id_max
+      from word_type wt
+               join word w on wt.id = w.type_id
+      where speech_part = 'verb_intransitive_terminative'
+      group by wt.speech_part, wt.id) word_from_word_type
+         join word w2
+              on word_from_word_type.word_id_max = w2.id
+         join derivative_form df
+              on w2.id = df.base_word_id
+                  and df.description = 'мин.св.вр., 2л., ед.ч.'
+         join derivative_form df3
+              on w2.id = df3.base_word_id
+                  and df3.description = 'мин.деят.св.прич. м.р.'
+         join derivative_form df4
+              on w2.id = df4.base_word_id
+                  and df4.description = 'мин.деят.св.прич. мн.ч.'
+         join derivative_form df5
+              on w2.id = df5.base_word_id
+                  and df5.description = 'мин.деят.несв.прич. м.р.'
+where df.name || 'л' <> df3.name
+   or df.name || 'ли' <> df4.name
+order by word_from_word_type.speech_part, word_from_word_type.id ;
